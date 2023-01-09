50c040ecd2ba3f5147788daf342dade88863b3ff
ruwey <gd@term>
2023-01-09 16:43:49 -0500

    added gaming stuff

--- users/ruwey.nix
+++ users/ruwey.nix
@@ -16,10 +16,37 @@ inp@{ home, pkgs, osConfig, ... }:
     foliate
     gnome.nautilus
     qalculate-gtk gnuplot
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     signal-desktop
     spotify
     steam
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
     zathura
+
+    # Gaming
+    (prismlauncher.override {
+      glfw = let
+        mcWaylandPatchRepo = fetchFromGitHub {
+          owner = "Admicos";
+          repo = "minecraft-wayland";
+          rev = "370ce5b95e3ae9bc4618fb45113bc641fbb13867";
+          sha256 =
+            "sha256-RPRg6Gd7N8yyb305V607NTC1kUzvyKiWsh6QlfHW+JE=";
+        };
+        mcWaylandPatches = map (name: "${mcWaylandPatchRepo}/${name}")
+          (lib.naturalSort (builtins.attrNames (lib.filterAttrs
+            (name: type:
+              type == "regular" && lib.hasSuffix ".patch" name)
+            (builtins.readDir mcWaylandPatchRepo))));
+      in glfw-wayland.overrideAttrs (previousAttrs: {
+        patches = previousAttrs.patches ++ mcWaylandPatches;
+      });
+    })
+    steam
 
     # Emacs Adjacent
     ((emacsPackagesFor (inp.emacs-overlay.packages."x86_64-linux".emacsPgtk))
